5d7a644c5035d3100f95c49e2d72a0338124e423
aicha709 <aychakhazen3@gmail.com>
2021-09-28 14:29:24 +0100

    Search Algorithms

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,16 +1,10 @@
-#ifndef SEARCH_ALGOS
-#define SEARCH_ALGOS
+#ifndef _BINARY_SEARCH_
+#define _BINARY_SEARCH_
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-int help_binary(int *array, int value, size_t lo, size_t hi);
-void array_print(int *array, size_t lo, size_t hi);
-int jump_search(int *array, size_t size, int value);
-int advanced_binary(int *array, size_t size, int value);
-int exponential_search(int *array, size_t size, int value);
 
 #endif
